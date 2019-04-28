22cca70e6019724c833db4e782a563ae37a477bd
student12 <student12@pcoip.ki.agh.edu.pl>
2019-04-28 11:48:28 +0200

    hashing

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,6 +2,9 @@ package com.company.enroller.persistence;
 
 import com.company.enroller.model.Participant;
 import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
@@ -10,6 +13,9 @@ import java.util.Collection;
 public class ParticipantService {
 
     DatabaseConnector connector;
+    
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     public ParticipantService() {
         connector = DatabaseConnector.getInstance();
@@ -22,8 +28,10 @@ public class ParticipantService {
     public Participant findByLogin(String login) {
         return (Participant) connector.getSession().get(Participant.class, login);
     }
-
+    
     public Participant add(Participant participant) {
+    	String hashedPassword = passwordEncoder.encode(participant.getPassword());
+    	participant.setPassword(hashedPassword);
         Transaction transaction = connector.getSession().beginTransaction();
         connector.getSession().save(participant);
         transaction.commit();
